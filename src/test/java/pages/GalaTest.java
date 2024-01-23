78232bea3303e7afb982fb104bc1dc3781a071d8
RafaCand <martincandilejo@hotmail.com>
2024-01-23 00:36:57 -0500

    Prueba Galavail

--- src/test/java/pages/GalaTest.java
+++ src/test/java/pages/GalaTest.java
@@ -1,13 +1,13 @@
 package pages;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class webProd {
+public class GalaTest {
 
     private WebDriver driver;
 
@@ -21,9 +21,8 @@ public class webProd {
     @Test
     public void navegamosgala() {
         //Abre la pagina de google
-        driver.get("galavail.com");
-        //Imprime el titulo de la pagina
-        System.out.println("Titulo de la pagina: " + driver.getTitle());
+        driver.get("https://develop.galavail.com/galavail-web/login.xhtml");
+        
         }
 
     @AfterMethod
